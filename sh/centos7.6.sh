23396447f462995acd35e608bfb0d561fea06222
blackdog <1471175708@qq.com>
2022-04-16 22:27:30 +0800

    modified centos7.6.sh

--- sh/centos7.6.sh
+++ sh/centos7.6.sh
@@ -9,6 +9,12 @@ sed -i "s/SELINUX=enforcing/SELINUX=disabled/g" /etc/selinux/config
 sed -i "s/#Port 22/Port 10888/g" /etc/ssh/sshd_config
 # 重启sshd
 systemctl restart sshd
+# firewalld开放sshd端口
+firewall-cmd --add-port=10888/tcp --permanent
+# firewalld开放80，443端口
+firewall-cmd --add-port=80/tcp --add-port=443/tcp --permanent
+# 重新加载firewalld配置
+firewall-cmd --reload
 
 # 安装netstat,lsof等网络工具
 yum install -y net-tools lsof
